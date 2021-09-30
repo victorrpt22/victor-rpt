211cebc70d158843af24b3fcd0a42efa4d32d4a0
Victor Perez <victor.rpt22@gmail.com>
2021-09-29 20:57:25 -0700

    added hamburguer menu

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,21 @@
 export default function NavBar(){
     return (
-        <nav className="nav nav-fill page-nav">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#">About</a>
-            <a className="nav-link" href="#">Skills</a>
-            <a className="nav-link" href="">Experience</a>
-            <a className="nav-link" href="">Blog</a>
-            <a className="nav-link" href="">Contact</a>
-        </nav>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+                <a className="navbar-brand" href="#">LoWGow</a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="container">
+                        <ul className="navbar-nav nav justify-content-center  nav-fill">
+                            <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Experience</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Blog</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
     );
 }
